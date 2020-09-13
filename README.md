d7acb7074ad4e83c4541a785f843180445efd3be
Josh <jdvswimmer@gmail.com>
2020-09-12 22:50:18 -0600

    Updates regex to handle bigger numbers

--- README.md
+++ README.md
@@ -21,3 +21,9 @@ To look like this:
 ## Reason
 
 If you import a lot of setlists they could have different formating. There are also a lot of songs that you don't want to play, so to look for the songs easier I wanted to get rid of the 150+ songs that had this formatting so that I could find the Artist and Song names easier. 
+
+## Troubleshooting
+Typically I get this error: 
+`"script" cannot be loaded because running scripts is disabled on this system. For more information, see about_Execution_Policies at https:/go.microsoft.com/fwlink/?LinkID=135170.`
+
+I believe you can override this for the session of your window with this command: `Set-ExecutionPolicy Bypass -Scope Process` but it hasn't been super consistent for me. 
--- rename-clonehero-folders.ps1
+++ rename-clonehero-folders.ps1
@@ -1,8 +1,8 @@
-$folders = Get-ChildItem -Path 'C:\Clone Hero\clonehero-win64\songs' | Where-Object {$_.PSIsContainer -eq $true} | Select-Object FullName, Name
+$folders = Get-ChildItem -Path "\\192.168.0.251\CloneHero\Clone Hero\clonehero-win64\songs" | Where-Object {$_.PSIsContainer -eq $true} | Select-Object FullName, Name
 
 foreach ($folder in $folders) {
-    if ($folder.Name -match '(\d\d - |\d\. )') {
-        Write-Output ("Replacing '$($folder.Name)' with '$($folder.Name -replace '(\d\d - |\d\. )', '')'")
-        Rename-Item -Path $folder.FullName -NewName $($folder.Name -replace '(\d\d - |\d\. )', '')
+    if ($folder.Name -match '^(\d\d - |\d+\. )') {
+        Write-Output ("Replacing '$($folder.Name)' with '$($folder.Name -replace '^(\d+ - |\d+\. )', '')'")
+        Rename-Item -Path $folder.FullName -NewName $($folder.Name -replace '^(\d+ - |\d+\. )', '')
     }
 }
